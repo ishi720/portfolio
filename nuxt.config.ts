2836771fc22f361ce058ca0344dd4d13fe73330e
ishi720 <ishizuka720@gmail.com>
2025-12-24 19:33:00 +0900

    ファビコン追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
       link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/portfolio/images/favicon.ico' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Poppins:wght@400;500;600;700&display=swap' }
       ]
     }
